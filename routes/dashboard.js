0f4de017c80af1bb8010fc9559c5d413a17e757a
Abderahim <ita27rmp100@gmail.com>
2025-01-30 00:46:47 +0100

    dashboard ... 95% completed

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { stat } = require('fs');
 var router = express.Router();
 const sql = require("mysql");
 
@@ -9,11 +10,37 @@ let connection = sql.createConnection({
   database: 'linuxhub'
 });
 
-router.get('/',function(req,res,next){
-    let MessagesTable = ''
-    connection.query('select * from form',function(err,result,fields){
-        messages = result
-        for(let i=0;i<(Object.keys(messages).length);i++){
+router.get('/',function(req,res){
+    let MessagesTable = '' , learnsTable = ''
+    connection.query('select * from form,testlinks,learncontent,statistics',function(err,result,fields){
+        console.log(result)
+        let messages = [] , learns = [] , message = '',content = ''
+        let lnks = new Array(result[0].link,result[1].link,result[2].link,result[3].link)
+        for(let i=0;i<Object.keys(result).length;i++){
+            messages.push({
+              fullName:result[i].fullName,
+              email:result[i].email,
+              message:result[i].message,
+            })
+            learns.push({
+                title:result[i].title,
+                etype:result[i].etype,
+                lang:result[i].lang,
+                img:result[i].img,
+                link:result[i].lnk,
+            })
+        }
+        // filtering
+        messages = messages.filter((value, index, self) =>
+            index === self.findIndex((t) => t.email === value.email && t.fullName === value.fullName && t.message === value.message)
+        );
+        learns = learns.filter((value, index, self) =>
+            index === self.findIndex((t) => 
+                t.title === value.title && t.etype === value.etype && t.lang === value.lang && t.img === value.img && t.link === value.link
+            )
+        );
+        // HTML code generating
+        for(i=0;i<(Object.keys(messages).length);i++){
             message=`<tr>
                         <td>${messages[i].fullName}</td>
                         <td>${messages[i].email}</td>
@@ -26,8 +53,28 @@ router.get('/',function(req,res,next){
                     </tr>`
             MessagesTable += message
         }
-        MessagesTable += `<tfoot><tr><th colspan="3">Total messages</th><td>${Object.keys(message).length}</td></tfoot>`
-        res.render('dashboard',{UsersMessages: MessagesTable})
+        MessagesTable += `<tfoot><tr><th colspan="3">Total messages</th><td>${Object.keys(messages).length}</td></tfoot>`
+        for(i=0;i<(Object.keys(learns).length);i++){
+            content = `<tr>
+                        <td>${learns[i].title}</td>
+                        <td>${learns[i].etype}</td>
+                        <td>${learns[i].lang}</td>
+                        <td>${learns[i].link}</td>
+                        <td>${learns[i].img}</td>
+                    </tr>`
+            learnsTable += content
+        }
+        console.log(result)
+        stcs=`<tr>
+                <td>${result[0].val}<t/d>
+                <td>${result[8].val}</td>
+                <td>${result[4].val}</td>
+                <td>${result[12].val}</td>
+            </tr>`
+        res.render('dashboard',{
+            UsersMessages: MessagesTable,
+            lnks,learnsTable,stcs
+        })
     })
 })
 
